f9a7de4a081fe2332d4a3d0519c84e0efcc7bd38
Jovincent <ino20001@byui.edu>
2022-01-07 01:08:43 +0800

    initial commit

--- JavaScript/function.js
+++ JavaScript/function.js
@@ -1,3 +1,10 @@
+var d = new Date(document.lastModified);
+var m = d.getMonth() + 1;
+var date = d.getDate();
+var y = d.getFullYear();
+var h = d.getHours();
+var min = d.getMinutes();
+var s = d.getSeconds();
 window.onload = function what(){
-    document.getElementById('latestDate').innerHTML = `Last Updated: ${new Date(document.lastModified)}`;
+    document.getElementById('latestDate').innerHTML = `Last Update: ${m}/${date}/${y} ${h}:${min}:${s}`;
     };
