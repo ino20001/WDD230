2016be9027c1cc24e77da2ed139b685a937255c7
Jovincent <ino20001@byui.edu>
2022-01-27 07:49:35 +0800

    draft

--- chamber/js/today.js
+++ chamber/js/today.js
@@ -1,11 +1,8 @@
-// select the elements to manipulate (output to)
-const datefield = document.querySelector("date");
 
-// derive the current date using a date object
 const now = new Date();
 const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(
 	now
 );
-// long, medium, short options ... try them
-
-datefield.innerHTML = `<em>${fulldate}</em>`;
+window.onload = function what(){
+document.getElementById("today").innerHTML = `<em>${fulldate}</em>`;
+}
