e855b92d672f2fa2e1a9ee143f023bd2d0f46bc5
Jovincent <ino20001@byui.edu>
2022-01-27 08:03:05 +0800

    draft

--- chamber/js/script.js
+++ chamber/js/script.js
@@ -5,6 +5,11 @@ let y = d.getFullYear();
 let h = d.getHours();
 let min = d.getMinutes();
 let s = d.getSeconds();
+
+const now = new Date();
+const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(
+	now);
 window.onload = function what(){
-    document.getElementById('latestDate').innerHTML = `Last Modification: ${m}/${date}/${y} ${h}:${min}:${s}`;
+    document.getElementById("today").innerHTML = `<em>${fulldate}</em>`;
+    document.getElementById('latestDate').innerHTML = `Last Update: ${m}/${date}/${y} ${h}:${min}:${s}`;
     };
--- chamber/js/today.js
+++
@@ -1,8 +0,0 @@
-
-const now = new Date();
-const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(
-	now
-);
-window.onload = function what(){
-document.getElementById("today").innerHTML = `<em>${fulldate}</em>`;
-}
