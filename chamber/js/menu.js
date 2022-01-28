a7245f7f6bae2e5eb4c42eb62c945c5d8f850a2b
Jovincent <ino20001@byui.edu>
2022-01-28 15:28:42 +0800

    Final ?

--- chamber/js/menu.js
+++ chamber/js/menu.js
@@ -1,7 +1,6 @@
 function toggleMenu() {
-    document.getElementById("linklist").classList.toggle("open");
-    document.getElementById("hambttn").classList.toggle("open");
-  }
-  
-  const x = document.getElementById('hambttn');
-  x.onclick = toggleMenu;
+  var para = document.getElementById("hambttn");
+  para.classList.toggle("rotate-icon");
+  document.getElementById("linklist").classList.toggle("open");
+  document.getElementById("hambttn").classList.toggle("open");
+}
