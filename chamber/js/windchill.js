5c82de6921bccfde9c72a11f1b91579f3bc20c6b
Jovincent <ino20001@byui.edu>
2022-02-12 21:03:13 +0800

    test one

---
+++ chamber/js/windchill.js
@@ -0,0 +1,11 @@
+const windspeed = parseFloat(document.querySelector("#speed").innerHTML)
+
+
+
+if(temp <= 50 && windspeed > 3){
+    let windchill = (35.74 + (0.6215 * temp))-(35.75 * Math.pow(windspeed,0.16)) + (0.4275*temp*Math.pow(windspeed,0.16));
+    windchill = Math.round((windchill + Number.EPSILON) * 10) / 10;
+    document.querySelector("#windchill").innerHTML = `${windchill}&#176;F`;
+} else{
+    "N/A"
+}
