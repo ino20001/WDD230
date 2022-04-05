9a2e1cfd595c475c4b974f0a0356e6cd4dcd6de0
Jovincent <ino20001@byui.edu>
2022-04-05 23:59:25 +0800

    likes fail

--- final-project/js/temples.js
+++ final-project/js/temples.js
@@ -22,19 +22,28 @@ fetch(requestURL)
         let schedlinks = document.createElement("a");
         let button = document.createElement("button");
         let lbutton = document.createElement("button");
+        let likebutton = document.createElement("button");
+        let numberoflikes = document.createElement("p");
 
         portrait.setAttribute('scr', temples.imageurl);
         portrait.setAttribute('alt', 'Photo of ' + temples.templename);
         h2.textContent = temples.templename;
-        address.innerHTML = `Address: ${temples.address}`
-        Tel.innerHTML = `Telephone: ${temples.Tel}`
-        lbutton.innerHTML = `More info`
+        address.innerHTML = `${temples.address}`
+        address.setAttribute ('class', `temaddress`);
+        Tel.innerHTML = `<b>Telephone:</b> ${temples.Tel}`
+        lbutton.innerHTML = `<b>More info</b>`
         links.setAttribute ('href', temples.link);
         schedlinks.setAttribute ('href', temples.schedapplink)
         address.innerHTML = `${temples.address}`
-        email.innerHTML = `Email: ${temples.email}`
-        button.innerHTML = `Schedule An Appointment`
-        services.innerHTML = `Services: ${temples.services}`
+        email.innerHTML = `<b>Email:</b> ${temples.email}`
+        button.innerHTML = `<b>Schedule An Appointment</b>`
+        services.innerHTML = `<b>Services:</b> ${temples.services}`
+        likebutton.innerHTML = `Like &#9825;`
+        numberoflikes.innerHTML = "";
+        numberoflikes.setAttribute ('class', `root`);
+        likebutton.setAttribute ('class', `like`);
+        likebutton.setAttribute ( 'onclick', `plusOne()`)
+
 
         card.appendChild(portrait);
         card.appendChild(h2);
@@ -47,5 +56,7 @@ fetch(requestURL)
         card.appendChild(schedlinks);
         schedlinks.appendChild(button);
         card.appendChild(schedlinks);
+        card.appendChild(likebutton);
+        card.appendChild(numberoflikes)
         templelist.appendChild(card);
     }
