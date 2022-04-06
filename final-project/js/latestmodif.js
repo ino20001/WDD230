969d9e62b400a315af03b0d95773c4589bada08b
Jovincent <ino20001@byui.edu>
2022-04-07 02:50:38 +0800

    recep done

--- final-project/js/latestmodif.js
+++ final-project/js/latestmodif.js
@@ -1,5 +1,6 @@
 let d = new Date(document.lastModified);
-let m = d.getMonth() + 1;
+const months = ["January","February","March","April","May","June","July","August","September","October","November","December"];
+let m = months[d.getMonth()];
 let date = d.getDate();
 let y = d.getFullYear();
 let h = d.getHours();
@@ -9,4 +10,8 @@ let s = d.getSeconds();
 
 window.onload = function what(){
 document.getElementById("latestDate").innerHTML = `Last Update: ${m}/${date}/${y} ${h}:${min}:${s}`;
+document.getElementById("today").innerHTML = `${m} ${date}`;
+document.getElementById("tomorrow").innerHTML = `${m} ${date + 1}`;
+document.getElementById("day2").innerHTML = `${m} ${date + 2}`;
+document.getElementById("day3").innerHTML = `${m} ${date + 3}`;
 }
