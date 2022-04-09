f2556b0bf07e5afe2e6f281060b090d96debccd1
Jovincent <ino20001@byui.edu>
2022-04-09 21:45:54 +0800

    Done List view

--- chamber/js/directory.js
+++ chamber/js/directory.js
@@ -1,5 +1,6 @@
 const requestURL = 'https://ino20001.github.io/wdd230/chamber/data/data.json';
-const directlist = document.querySelector(".directList");
+const directcard = document.querySelector(".Card");
+const directList = document.querySelector(".List");
 fetch(requestURL)
   .then(function (response) {
     return response.json();
@@ -11,30 +12,66 @@ fetch(requestURL)
 
   
   function displayDirectory(directory) {
+    //card view
     let card = document.createElement('section');
     let h2 = document.createElement('h2');
     let portrait = document.createElement('img');
     let info = document.createElement("p");
     let address = document.createElement("p");
     let links = document.createElement("a");
+    //listview
+    let tr = document.createElement('tr');
+    let tdname = document.createElement('td');
+    let tdadd = document.createElement('td');
+    let tdtel = document.createElement('td');
+    let tdlinks = document.createElement('td');
+    let tdlink = document.createElement('a');
+    
   
-    h2.textContent = directory.companyname;
 
+    //card view
+    h2.textContent = directory.companyname;
     address.innerHTML = `${directory.address}`
     info.innerHTML = `Telephone: ${directory.Tel}`
     links.innerHTML = `${directory.link}`
-
     links.setAttribute('href', directory.link);
+    links.setAttribute('target', `_blank`);
     portrait.setAttribute('src', directory.imageurl);
     portrait.setAttribute('alt', 'Logo of ' + directory.name);
     portrait.setAttribute('loading', 'lazy');
+    //list view
+    tdname.textContent = directory.companyname;
+    tdadd.innerHTML = `${directory.address}`;
+    tdtel.textContent = directory.Tel;
+    tdlink.innerHTML = `Link here`;
+    tdlink.setAttribute('href', directory.link);
+    tdlink.setAttribute('target', `_blank`);
 
+
+
+   //cardview
     card.appendChild(h2);
     card.appendChild(portrait);
     card.appendChild(address);
     card.appendChild(info);
     card.appendChild(links);
-    directlist.appendChild(card);
+    directcard.appendChild(card);
+    //listView
+
+    tr.appendChild(tdname);
+    tr.appendChild(tdadd);
+    tr.appendChild(tdtel);
+    tr.appendChild(tdlinks);
+    tdlinks.appendChild(tdlink);
+    directList.appendChild(tr);
+  };
+
+
+  function toggleView() {
+    const list = document.getElementById("viewOpt");
+    list.classList.toggle("listView");
+    document.querySelector('.directOpt').classList.toggle("view");
+    document.querySelector('.viewOpt').classList.toggle("view");
   }
   
   
